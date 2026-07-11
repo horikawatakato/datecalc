b58f555a38c10c774d2ee8f6552c6ae338f8694a
horikawatakato <takahorikawa@icloud.com>
2026-07-11 15:01:52 +0900

    Update Dockerfile/gunicorn.conf

--- app/gunicorn.conf.py
+++ app/gunicorn.conf.py
@@ -1,11 +1,11 @@
 import os
 
-bind        = "0.0.0.0:8000"
-workers     = int(os.environ.get("WEB_CONCURRENCY", 2))  # コンテナ資源に合わせ固定
-worker_class = "sync"
-timeout     = 120
-accesslog   = "-"   # stdout
-errorlog    = "-"   # stderr
-loglevel    = "info"
+bind                = "0.0.0.0:8000"
+workers             = int(os.environ.get("WEB_CONCURRENCY", 2))  # CPU数から自動算出せず固定
+worker_class        = "sync"
+timeout             = 30
+accesslog           = "-"   # stdout
+errorlog            = "-"   # stderr
+loglevel            = "info"
 max_requests        = 1000
 max_requests_jitter = 50
